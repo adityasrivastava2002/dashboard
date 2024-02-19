a1749c04c184ffdf0349b4b4ceb8dfa7a1b3ec8c
Aditya <adityasrivastava2002@gmail.com>
2024-02-19 23:36:04 +0530

    fixed changes

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="px-20 py-10 w-[72%] max-lg:h-[100%] max-lg:w-[100%] max-sm:mx-auto">
       <h1 className="text-2xl text-white max-xl:text-center">Hey, <span className="text-yellow-new">Name!</span></h1>
-      <h1 className="text-2xl text-white max-xl:text-center">How's your prep for the, <span className="text-yellow-new">March SAT</span></h1>
+      <h1 className="text-2xl text-white max-xl:text-center">How&#39;s your prep for the, <span className="text-yellow-new">March SAT</span></h1>
       <div className="flex flex-wrap gap-6 py-6 max-xl:justify-center">
         {
           cardData.map((data,i)=>(
